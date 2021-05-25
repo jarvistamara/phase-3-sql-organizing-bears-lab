1d43edbbe7e72d927e64eded21718d042bc1f399
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-05-24 21:26:15 -0400

    finally passed woohoo

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chocolate", 1, "M", "black", "protective", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 5, "M", "brown", "calm", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 7, "F", "white", "intense", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergeant Brown", 2, "M", "black", "aggressive", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 3, "F", "black", "mean", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 9, "M", "brown", "dangerous", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 2, "F", "black", "scared", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, null, 1, "M", "brown", "protective", 0);
